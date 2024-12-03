ebcc8cb29e720311f9479ba6e3fd96c015c80616
Oluwatoyin <toyeentop@yahoo.com>
2024-12-02 21:04:35 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,19 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="">
+        <h2>Welcome to Oluwatoyin's Personal Website</h2>
+        <h3>Contact me: Oluwatoyin@yahoo.com</h3>
+        <h3><a href='https://www.linkedin.com' target='_blank'>LinkedIn</a></h3>
+        <h3><a href='https://www.github.com' target='_blank'>Github</a></h3>
+
+
       </header>
     </div>
   );
