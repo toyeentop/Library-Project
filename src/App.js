cc1b8078cffcef48702930e437c21b98ee8e6c92
Oluwatoyin <toyeentop@yahoo.com>
2024-12-07 11:37:58 -0500

    updated email address

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="">
         <h2>Welcome to Oluwatoyin's Personal Website</h2>
-        <h3>Contact me: Oluwatoyin@yahoo.com</h3>
+        <h3>Contact me: toyeentop@yahoo.com</h3>
         <h3><a href='https://www.linkedin.com' target='_blank'>LinkedIn</a></h3>
         <h3><a href='https://www.github.com' target='_blank'>Github</a></h3>
 
